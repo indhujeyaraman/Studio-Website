4bf49cd137ec23a16af852873243cf90fe9b6ea1
Indhu <indhujayk@gmail.com>
2025-06-16 19:08:00 +0530

    Added First section

--- app/page.js
+++ app/page.js
@@ -1,7 +1,30 @@
-import React from "react";
+import HeroSection from "@/component/HeroSection";
 
-const page = () => {
-  return <div>page</div>;
+import heroImage from "../public/images/heroImage.jpg"
+import React from "react";
+import Image from "next/image";
+const Home = () => {
+  return (
+    <main className="bg-">
+    
+      <Image src={heroImage} alt="bg" width={1000} height={1000} />
+      <div>
+        <h2 className="text-dark">LITTLE SPOTLIGHT ON US</h2>
+        <h3>why we are the talk of the town?</h3>
+        <p>
+          Yes, we’re seasoned pros - hundreds of clients across multiple
+          destinations can vouch for that. Yes, we’re ‘highly acclaimed’ (just
+          take a peek at our glowing Google reviews). But here’s the real deal -
+          what truly matters to us is delivering an experience like no other.
+          Your photos won’t just be stunning—they’ll be unforgettable. And the
+          journey with us? From the first hello to the final shot, it’s designed
+          to be nothing short of extraordinary.
+        </p>
+
+      </div>
+      <HeroSection />
+    </main>
+  );
 };
 
-export default page;
+export default Home;
---
+++ component/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <ul><li>HOME</li></ul>
+  )
+}
+
+export default Navbar
